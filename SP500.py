ab094ba7e54eeb9fb510a05b3c186c7981e8ab7e
Z <45545234+zacharimayer@users.noreply.github.com>
2019-04-17 18:17:17 -0400

    Resolving broken Yahoo Finance API issue

--- SP500.py
+++ SP500.py
@@ -21,3 +21,28 @@ def save_sp500_tickers():
     return tickers
 
 save_sp500_tickers()
+
+
+def get_data_from_yahoo(reload_sp500=False):
+
+    if reload_sp500:
+        tickers = save_sp500_tickers()
+    else:
+        with open("sp500tickers.pickle", "rb") as f:
+            tickers = pickle.load(f)
+
+    if not os.path.exists('stock.dfs'):
+        os.makedirs('stock_dfs')
+
+    start = dt.datetime(2000,1,1)
+    end = dt.datetime(2019,1,1)
+
+    for ticker in tickers:
+        print(ticker)
+        if not os.path.exists('stockdfs/{}.csv'.format(ticker)):
+            df = web.DataReader(ticker, 'yahoo', start, end)
+            df.to_csv('stockdfs/{}.csv'.format(ticker))
+        else:
+            print('Already have {}'.format(tickers))
+
+get_data_from_yahoo()
