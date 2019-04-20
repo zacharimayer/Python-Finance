a6951fd27f8cbceaa41414e59c06e1b9bdb164af
Z <45545234+zacharimayer@users.noreply.github.com>
2019-04-20 18:02:24 -0400

    Expanded libs

--- create-pickle.py
+++ create-pickle.py
@@ -1,15 +1,21 @@
-import bs4 as bs
 import datetime as dt
-import os
+import matplotlib.pyplot as plt
+from matplotlib import style
+from mpl_finance import candlestick_ohlc
+import matplotlib.dates as mdates
 import pandas as pd
-import pandas_datareader
 import pandas_datareader.data as web
-from pandas_datareader import data as pdr
-import fix_yahoo_finance as yf
-yf.pdr_override()
+import datetime as dt
+from datetime import datetime, timedelta
+import bs4 as bs
 import pickle
 import requests
-import fix_yahoo_finance as yf
+import os
+import numpy as np
+from collections import Counter
+from sklearn import svm, cross_validation, neighbors
+from sklearn.ensemble import VotingClassifier, RandomForestClassifier
+from numpy.core.umath_tests import inner1d
 
 def save_sp500_tickers():
     resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
--- get_data_to_csv.py
+++ get_data_to_csv.py
@@ -1,8 +1,21 @@
 import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style
+from mpl_finance import candlestick_ohlc
+import matplotlib.dates as mdates
 import pandas as pd
 import pandas_datareader.data as web
+import datetime as dt
+from datetime import datetime, timedelta
+import bs4 as bs
+import pickle
+import requests
+import os
+import numpy as np
+from collections import Counter
+from sklearn import svm, cross_validation, neighbors
+from sklearn.ensemble import VotingClassifier, RandomForestClassifier
+from numpy.core.umath_tests import inner1d
 
 style.use('ggplot')
 
